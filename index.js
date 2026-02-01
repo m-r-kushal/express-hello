9093e5cdafae84ae1884479280436913964a4f90
A. F. M. Mahbubur Rahman <m.r.kushal@gmail.com>
2026-02-01 11:00:32 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const {engine} = require('express-handlebars');
+const express = require('express')
+
+const app = express();
+
+
+app.engine('handlebars', engine());
+
+// if you want to disable the default layout, you can set defaultLayout to false
+// check the line below. comment the previous app.engine line and uncomment
+// the one below to see the effect
+//app.engine('handlebars', engine({defaultLayout:false}));
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Home Page', message: 'Welcome to the Home Page!'});
+});
+
+app.get('/about', (req, res) => {
+
+  const info = {
+    title: 'About Page', message: 'Welcome to the About Page!',
+
+    'address': {
+      'street': '123 Main St',
+      'city': 'Anytown',
+      'state': 'CA',
+      'zip': '12345'
+    },
+    name: 'John Doe',
+    age: 30,
+    hobbies: ['reading', 'traveling', 'coding']
+
+  };
+
+  res.render('about', info);
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`);
+});
